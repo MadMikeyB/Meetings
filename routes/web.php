a99e252b473a9fca8c867dc47ea763bbc621cf93
Jack Ellis <jackellis1504@gmail.com>
2020-02-22 10:07:19 +0000

    New plan route

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,8 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::get('/plan', 'MeetingController@newPlan');
+
 //Route::resource('company', 'CompanyController');
 //Route::resource('meeting', 'MeetingController');
 //Route::resource('agendaitem', 'AgendaItemController');
