14d5bf2acbba0a3f97d831e3f4994a07893bfe0e
Jack Ellis <jackellis1504@gmail.com>
2020-02-22 09:38:33 +0000

    Moved user api route to api namespace

--- routes/api.php
+++ routes/api.php
@@ -13,11 +13,10 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::namespace("Api")->name("api.")->group(function() {
+    Route::middleware('auth:api')->get('/user', function (Request $request) {
+        return $request->user();
+    })->name('user');
     Route::apiResource('company', 'CompanyController');
     Route::apiResource('meeting', 'MeetingController');
     Route::apiResource('agendaitem', 'AgendaItemController');
