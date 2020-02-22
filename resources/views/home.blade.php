9aa3d617f9c0617a445b207e46fafc649a416666
Jack Ellis <jackellis1504@gmail.com>
2020-02-22 09:41:07 +0000

    Rearrangement

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,30 @@
-@extends('layouts.app')
+@extends('layouts.base')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+@section('title', 'Dashboard')
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+@php
+$meetings = [];
 
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
+for($i = 0; $i < 10; $i++) {
+  $meetings[] = [
+    "title" => "Meeting ".$i,
+    "series" => "Series ".$i,
+    "location" => "Location ".$i,
+    "organiser" => "Organiser ".$i,
+    "time" => "Time ".$i,
+  ];
+}
+
+
+@endphp
+
+@section('headline')
+  You are logged in
+@endsection
+
+@section('main')
+<div class="dashboard container">
+  @include('includes.my_meetings')
+  @include('includes.my_next_steps')
 </div>
 @endsection
