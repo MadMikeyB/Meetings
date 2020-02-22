2af207f47305fc9b4ab603eb41bdf98d551d74db
Jack Ellis <jackellis1504@gmail.com>
2020-02-22 11:10:50 +0000

    Add factories to seeding

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -4,13 +4,31 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
-    }
+  /**
+   * Seed the application's database.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    $companies = factory(App\Company::class, 5)
+      ->create()
+      ->each(function ($company) {
+        factory(App\User::Class, 3)
+          ->create(['company_id' => $company->id])
+          ->each(function($user) {
+            factory(App\Meeting::Class, 3)
+              ->create(['user_id' => $user->id])->each(function($meeting) {
+                $benefits = factory(App\Benefit::class, 3)->create(['meeting_id' => $meeting->id]);
+                $concerns = factory(App\Concern::class, 3)->create(['meeting_id' => $meeting->id]);
+                $decisions = factory(App\Decision::class, 3)->create(['meeting_id' => $meeting->id]);
+                $notes = factory(App\Note::class, 3)->create(['meeting_id' => $meeting->id]);
+
+              });
+          });
+      });
+
+    //$agenda_items = factory(App\AgendaItem::class)->create();
+    // $this->call(UsersTableSeeder::class);
+  }
 }
