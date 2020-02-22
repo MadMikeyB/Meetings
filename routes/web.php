b70c9ce749aee22221599cfdb1b476d4efafaa4d
Jack Ellis <jackellis1504@gmail.com>
2020-02-22 09:38:51 +0000

    Added redirect for home and commented all resources

--- routes/web.php
+++ routes/web.php
@@ -12,22 +12,23 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+  return redirect('/home');
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::resource('company', 'CompanyController');
-Route::resource('meeting', 'MeetingController');
-Route::resource('agendaitem', 'AgendaItemController');
-Route::resource('attendee', 'AttendeeController');
-Route::resource('benefit', 'BenefitController');
-Route::resource('concern', 'ConcernController');
-Route::resource('day', 'DayController');
-Route::resource('decision', 'DecisionController');
-Route::resource('expectation', 'ExpectationController');
-Route::resource('nextstep', 'NextStepController');
-Route::resource('note', 'NoteController');
-Route::resource('objective', 'ObjectiveController');
-Route::resource('token', 'TokenController');
+
+//Route::resource('company', 'CompanyController');
+//Route::resource('meeting', 'MeetingController');
+//Route::resource('agendaitem', 'AgendaItemController');
+//Route::resource('attendee', 'AttendeeController');
+//Route::resource('benefit', 'BenefitController');
+//Route::resource('concern', 'ConcernController');
+//Route::resource('day', 'DayController');
+//Route::resource('decision', 'DecisionController');
+//Route::resource('expectation', 'ExpectationController');
+//Route::resource('nextstep', 'NextStepController');
+//Route::resource('note', 'NoteController');
+//Route::resource('objective', 'ObjectiveController');
+//Route::resource('token', 'TokenController');
