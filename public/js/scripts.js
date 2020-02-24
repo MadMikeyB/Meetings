80dbd0978f486dd8563f6bb80c84a10af82e7b92
Jack Ellis <jackellis1504@gmail.com>
2020-02-24 15:31:28 +0000

    Compiled

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,98 +1,3 @@
-/******/ (function(modules) { // webpackBootstrap
-/******/ 	// The module cache
-/******/ 	var installedModules = {};
-/******/
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/
-/******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId]) {
-/******/ 			return installedModules[moduleId].exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = installedModules[moduleId] = {
-/******/ 			i: moduleId,
-/******/ 			l: false,
-/******/ 			exports: {}
-/******/ 		};
-/******/
-/******/ 		// Execute the module function
-/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/
-/******/ 		// Flag the module as loaded
-/******/ 		module.l = true;
-/******/
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/
-/******/
-/******/ 	// expose the modules object (__webpack_modules__)
-/******/ 	__webpack_require__.m = modules;
-/******/
-/******/ 	// expose the module cache
-/******/ 	__webpack_require__.c = installedModules;
-/******/
-/******/ 	// define getter function for harmony exports
-/******/ 	__webpack_require__.d = function(exports, name, getter) {
-/******/ 		if(!__webpack_require__.o(exports, name)) {
-/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
-/******/ 		}
-/******/ 	};
-/******/
-/******/ 	// define __esModule on exports
-/******/ 	__webpack_require__.r = function(exports) {
-/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 		}
-/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 	};
-/******/
-/******/ 	// create a fake namespace object
-/******/ 	// mode & 1: value is a module id, require it
-/******/ 	// mode & 2: merge all properties of value into the ns
-/******/ 	// mode & 4: return value when already ns object
-/******/ 	// mode & 8|1: behave like require
-/******/ 	__webpack_require__.t = function(value, mode) {
-/******/ 		if(mode & 1) value = __webpack_require__(value);
-/******/ 		if(mode & 8) return value;
-/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
-/******/ 		var ns = Object.create(null);
-/******/ 		__webpack_require__.r(ns);
-/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
-/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
-/******/ 		return ns;
-/******/ 	};
-/******/
-/******/ 	// getDefaultExport function for compatibility with non-harmony modules
-/******/ 	__webpack_require__.n = function(module) {
-/******/ 		var getter = module && module.__esModule ?
-/******/ 			function getDefault() { return module['default']; } :
-/******/ 			function getModuleExports() { return module; };
-/******/ 		__webpack_require__.d(getter, 'a', getter);
-/******/ 		return getter;
-/******/ 	};
-/******/
-/******/ 	// Object.prototype.hasOwnProperty.call
-/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
-/******/
-/******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/";
-/******/
-/******/
-/******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 0);
-/******/ })
-/************************************************************************/
-/******/ ({
-
-/***/ "./resources/js/scripts.js":
-/*!*********************************!*\
-  !*** ./resources/js/scripts.js ***!
-  \*********************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
-
 $(document).ready(function () {
   // Super simple tabbing
   $(document).on("click", ".tab-bar .tab", function (e) {
@@ -119,13 +24,15 @@ $(document).ready(function () {
     $(this).siblings(".tab-sort-filter__sorts").removeClass("active");
     $(this).siblings(".tab-sort-filter__filters").toggleClass("active");
   });
-  $(document).on("change", ".next-steps-ajax input", function () {
+  $(document).on("change", ".meetings-ajax input", function () {
+    console.log($(this));
     $.ajax({
       method: 'GET',
-      url: '/ajax/my_next_steps',
+      url: '/ajax/my_meetings',
       data: $("#mns-form").serialize(),
       success: function success(d, ts, xhr) {
-        $(".next-steps-ajax").html(d);
+        console.log("Meetings reloaded");
+        $(".meetings-ajax .list-group.tab-body-bar").html($(d).find(".list-group.tab-body-bar").html());
       },
       error: function error(x, t, e) {
         $(".ajax").html(x);
@@ -134,13 +41,15 @@ $(document).ready(function () {
       }
     });
   });
-  $(document).on("change", ".meetings-ajax input", function () {
+  $(document).on("change", ".next-steps-ajax input", function () {
+    console.log($(this));
     $.ajax({
       method: 'GET',
-      url: '/ajax/my_meetings',
+      url: '/ajax/my_next_steps',
       data: $("#mns-form").serialize(),
       success: function success(d, ts, xhr) {
-        $(".meetings-ajax").html(d);
+        console.log("Next Steps reloaded");
+        $(".next-steps-ajax .list-group.tab-body-bar").html($(d).find(".list-group.tab-body-bar").html());
       },
       error: function error(x, t, e) {
         $(".ajax").html(x);
@@ -156,31 +65,3 @@ $(document).ready(function () {
     $("[name=next_step\\[tab\\]]").val($(this).attr("tab-index"));
   });
 });
-
-/***/ }),
-
-/***/ "./resources/sass/app.scss":
-/*!*********************************!*\
-  !*** ./resources/sass/app.scss ***!
-  \*********************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
-
-// removed by extract-text-webpack-plugin
-
-/***/ }),
-
-/***/ 0:
-/*!*****************************************************************!*\
-  !*** multi ./resources/js/scripts.js ./resources/sass/app.scss ***!
-  \*****************************************************************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
-
-__webpack_require__(/*! /Users/jack/Sites/Meetings/resources/js/scripts.js */"./resources/js/scripts.js");
-module.exports = __webpack_require__(/*! /Users/jack/Sites/Meetings/resources/sass/app.scss */"./resources/sass/app.scss");
-
-
-/***/ })
-
-/******/ });
