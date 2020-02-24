3f4c54d0c8d8e1ba6fe74034550a2a01438a9547
Jack Ellis <jackellis1504@gmail.com>
2020-02-24 21:02:06 +0000

    Added multi-day inputs

---
+++ resources/views/includes/plan-day.blade.php
@@ -0,0 +1,23 @@
+@php
+
+if(isset($day)) {
+  $id = $day->id;
+  $date = $day->date->format('Y-m-d');
+  $start_at = $day->start_at;
+  $end_at = $day->start_at;
+} else {
+  $id = $day->id ?? $uuid;
+  $date = date("Y-m-d");
+  $start_at = date("H:i:s");
+  $end_at = date("H:i:s");
+}
+
+@endphp
+
+<fieldset class="days">
+  <input type="hidden" name="days[id][]" value="{{ $id }}">
+  <input type="date" name="days[date][]" value="{{ $date }}">
+  <input type="time" name="days[start_at][]" value="{{ $start_at }}">
+  <input type="time" name="days[end_at][]" value="{{ $end_at }}">
+</fieldset>
+
