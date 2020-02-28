ab01d5f4a50baeb6644d82180aced78a04def946
Jack Ellis <jackellis1504@gmail.com>
2020-02-28 17:51:03 +0000

    Added major bits of seeding

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,6 @@ class DatabaseSeeder extends Seeder
    */
   public function run()
   {
-
     $companies = factory(App\Company::class, 5)
       ->create()
       ->each(function ($company) {
@@ -62,7 +61,44 @@ class DatabaseSeeder extends Seeder
       'name' => 'Jack Ellis',
       'email' => 'jackellis1504@gmail.com',
       'password' => bcrypt(12345678),
-    ]);
+    ])->each(function($user) {
+            factory(App\Meeting::Class, 3)
+              ->create(['user_id' => $user->id])->each(function($meeting) {
+                $days = factory(App\Day::class, 2)->create([
+                  'meeting_id' => $meeting->id,
+                ])->each(function($day) {
+                  $items = factory(App\AgendaItem::class, 5)->create([
+                    'day_id' => $day->id,
+                    'leader_id' => App\User::all()->random()->id,
+                    'position' => 0,
+                  ]);
+                });
+                $benefits = factory(App\Benefit::class, 3)->create([
+                  'meeting_id' => $meeting->id
+                ]);
+                $concerns = factory(App\Concern::class, 3)->create([
+                  'meeting_id' => $meeting->id
+                ]);
+                $decisions = factory(App\Decision::class, 3)->create([
+                  'meeting_id' => $meeting->id
+                ]);
+                $notes = factory(App\Note::class, 3)->create([
+                  'meeting_id' => $meeting->id
+                ]);
+                $objectives = factory(App\Objective::class, 3)->create([
+                  'meeting_id' => $meeting->id
+                ]);
+                $expectations = factory(App\Expectation::class, 3)->create([
+                  'meeting_id' => $meeting->id
+                ]);
+                $nextsteps = factory(App\NextStep::class, 3)->create([
+                  'user_id' => App\User::all()->random()->id,
+                  'meeting_id' => $meeting->id,
+                ]);
+              });
+          });
+;
+
 
 
 
