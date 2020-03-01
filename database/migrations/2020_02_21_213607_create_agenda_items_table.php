758c28853e05db66631524d170f75dec17021994
Jack Ellis <jackellis1504@gmail.com>
2020-03-01 21:24:01 +0000

    Additional is now nullable, agenda item leader becomes just a string

--- database/migrations/2020_02_21_213607_create_agenda_items_table.php
+++ database/migrations/2020_02_21_213607_create_agenda_items_table.php
@@ -16,12 +16,12 @@ class CreateAgendaItemsTable extends Migration
         Schema::create('agenda_items', function (Blueprint $table) {
             $table->uuid('id');
             $table->uuid('day_id');
-            $table->uuid('leader_id');
+            $table->string('leader');
             $table->timestamps();
             $table->integer('type')->comment("See class definition for what the integer means");
             $table->text('name');
             $table->integer('position');
-            $table->text('additional');
+            $table->text('additional')->nullable();
             $table->integer('expected_number_of_minutes');
             $table->integer('actual_number_of_minutes')->default(0);
         });
