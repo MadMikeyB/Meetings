4644139aa81e715002b509aae9ada93a1ebebf55
Jack Ellis <jackellis1504@gmail.com>
2020-03-02 21:06:02 +0000

    Functions for adding and deleting agenda items

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Meeting;
 use App\NextStep;
+use App\Day;
+use App\AgendaItem;
 use Ramsey\Uuid\Uuid;
 use Illuminate\Support\Facades\Auth;
 
@@ -70,13 +72,62 @@ class AjaxController extends Controller
     );
   }
 
-  public function plan_add_agenda_item(Meeting $meeting)
+  public function plan_add_agenda_item(Day $day, int $item_type)
   {
-    //dump($meeting);
-    $uuid = Uuid::uuid4()->toString();
+    $meeting = $day->meeting;
+    $new_item = [
+      "id" => Uuid::uuid4()->toString(),
+      "day_id" => $day->id,
+      "position" => $day->agenda_items()->where('type', '>', AgendaItem::TYPE_OPEN)->first()->position - 0.5,
+      "type" => $item_type,
+      "expected_number_of_minutes" => 5,
+      "leader" => $meeting->user->id,
+    ];
+    switch($item_type) {
+    case AgendaItem::TYPE_NORMAL_ITEM:
+      $new_item["name"] = "New Agenda Item";
+      $new_item["expected_number_of_minutes"] = 15;
+      break;
+    case AgendaItem::TYPE_BREAK:
+      $new_item["name"] = "New Break";
+      break;
+    case AgendaItem::TYPE_LUNCH:
+      $new_item["name"] = "New Lunch";
+      break;
+    case AgendaItem::TYPE_ICE_BREAKER:
+      $new_item["name"] = "New Ice-Breaker";
+      break;
+    default:
+      break;
+    }
+
+    $new_item_object = AgendaItem::create($new_item);
+
+    $i = 0;
+    foreach($day->agenda_items as $ai) {
+      $ai->update(["position" => $i++]);
+    }
+
     return view(
-      'includes.plan.agenda_item',
-      compact(['uuid','meeting'])
+      'includes.agenda',
+      compact(['meeting'])
+    );
+  }
+
+  public function plan_delete_agenda_item(AgendaItem $item) {
+    $day = $item->day;
+    $meeting = $day->meeting;
+
+    $item->delete();
+
+    $i = 0;
+    foreach($day->agenda_items as $ai) {
+      $ai->update(["position" => $i++]);
+    }
+
+    return view(
+      'includes.agenda',
+      compact(['meeting'])
     );
   }
 
