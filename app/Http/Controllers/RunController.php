5ea23b08555fc698b3853a5e6bf36224c0377e44
Jack Ellis <jackellis1504@gmail.com>
2020-03-04 08:19:39 +0000

    Added auth facade and passing user into params

--- app/Http/Controllers/RunController.php
+++ app/Http/Controllers/RunController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Meeting;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class RunController extends Controller
 {
   public function choose() {
     $params = [];
     $meetings = [
-      'Upcoming Meetings' => Meeting::get_for_page($params)["Upcoming Meetings"],
+      'Upcoming Meetings' => Meeting::get_for_page($params, Auth::user())["Upcoming Meetings"],
     ];
 
     return view(
